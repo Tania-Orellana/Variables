fd737dae95ea392e2ad6b5151b497bd1753952d0
Tania Orellana <ivonneorre321@gmail.com>
2023-08-04 17:07:46 -0600

    segundo commit

--- Variable Python.py
+++ Variable Python.py
@@ -2,3 +2,6 @@ Entero = 4
 Float = 3.2
 Boolean = True
 String = "Hola"
+# Concatena a la cadena las otras variables aplicando la conversión correcta para funcionar, guarda el resultado en una variable
+resutado = String + str(Entero) +str (float) +str (boolean)
+print(resultado)
