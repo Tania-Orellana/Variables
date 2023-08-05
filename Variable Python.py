0acdbeb55a1f1cbfee277f5560e157cd17a82d7c
Tania Orellana <ivonneorre321@gmail.com>
2023-08-04 18:48:26 -0600

    Tercer commit

--- Variable Python.py
+++ Variable Python.py
@@ -2,6 +2,12 @@ Entero = 4
 Float = 3.2
 Boolean = True
 String = "Hola"
+
 # Concatena a la cadena las otras variables aplicando la conversión correcta para funcionar, guarda el resultado en una variable
 resutado = String + str(Entero) +str (float) +str (boolean)
 print(resultado)
+
+# Investiga sobre el límite de los enteros y los flotantes en python y anotar sus descubrimientos como comentarios en el archivo
+Entero= El valor de un número entero no está restringido por la cantidad de bits y puede expandirse hasta el límite de la memoria disponible
+Float = tienen valores minimo 2.2250738585072014e-308 y valores maximos 1.7976931348623157e+308
+
